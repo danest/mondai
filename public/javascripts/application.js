2b7f6c3eac738dce4e6e30ed03de50aefff0d0fc
Kevin Urrutia <urrutia.kevin@gmail.com>
2011-06-10 18:30:09 -0400

    added styling to homepage

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -7,14 +7,15 @@ $(document).ready(function() {
 	function popupCenter(url, width, height, name) {
 	  var left = (screen.width/2)-(width/2);
 	  var top = (screen.height/2)-(height/2);
-	  return window.open(url, name, "menubar=no,toolbar=no,status=no,width="+width+",height="+height+",toolbar=no,left="+left+",top="+top);
+	  window.open(url, name, "menubar=no,toolbar=no,status=no,width="+width+",height="+height+",toolbar=no,left="+left+",top="+top);
+	  return window.close();
 	}
 
 	$("a.popup").click(function(e) {
 	  popupCenter($(this).attr("href"), $(this).attr("data-width"), $(this).attr("data-height"), "authPopup");
 	  $('.signinsubmit').hide();
 	  $('#signup').css('margin-left', '300px');
-	  e.stopPropagation(); return false;
+	  e.stopPropagation(); 
+	  return false;
 	});
-	
 });
