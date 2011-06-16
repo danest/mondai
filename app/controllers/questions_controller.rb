585be6950dd35d70110d93173f7939cb44ade1e3
Kevin Urrutia <urrutia.kevin@gmail.com>
2011-06-15 23:11:23 -0400

    added show action

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -20,6 +20,15 @@ class QuestionsController < ApplicationController
     
   end
   
+  def show
+    @question = Question.find(params[:id])
+    respond_to do |format|
+        format.html # show.html.erb
+        format.xml  { render :xml => @post }
+    end
+  end
+  
+  
   
   #Allowing admin to delete question
   private
