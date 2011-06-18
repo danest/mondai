e23da2e2fc66cb34b2fb6087c55405cc59efac80
Norwich <nor201@gmail.com>
2011-06-18 10:53:34 -0400

    Fixed error #1 class name

---
+++ app/controllers/q_relationship_controller.rb
@@ -0,0 +1,16 @@
+class QRelationshipController < ApplicationController
+  before_filter :authenticate
+  respond_to :html, :js
+  
+  def create
+    @question = Question.find(params[:relationship][:q_followed_id])
+    current_user.follow_question!(@question)
+    respond_with @question
+  end
+  
+  def destroy
+    @question = QRelationship.find(params[:id]).question_followed
+    current_user.unfollow_question!(@question)
+    respond_with @question
+  end
+end
---
+++ app/controllers/u_relationship_controller.rb
@@ -0,0 +1,16 @@
+class URelationshipController < ApplicationController
+  before_filter :authenticate
+  respond_to :html, :js
+  
+  def create
+    @user = User.find(params[:relationship][:u_followed_id])
+    current_user.follow_user!(@user)
+    respond_with @user
+  end
+  
+  def destroy
+    @user = URelationship.find(params[:id]).user_followed
+    current_user.unfollow_user!(@user)
+    respond_with @user
+  end
+end
