1adca53466e873a9234eef62dee51240ebd364bf
Kevin Urrutia <urrutia.kevin@gmail.com>
2011-06-24 11:02:49 -0400

    fixed @friends bug in signup new url

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,11 @@ class HomeController < ApplicationController
   before_filter :authenticate_user!, :only => :show 
   
   def index
+    if !!current_user
+      render :action => 'logged_in_user'
+    else
+      render :action => 'new_user'
+    end
   end
 
   def show  
