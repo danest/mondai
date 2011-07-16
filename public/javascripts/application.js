2b522f075be17644fac58e4085fb755798a31c46
Kevin Urrutia <urrutia.kevin@gmail.com>
2011-07-16 16:03:46 -0400

    added new home page

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -8,8 +8,29 @@ jQuery.fn.center = function () {
     return this;
 }
 $(document).ready(function() {
+	var randomnumber=Math.floor(Math.random()*5)
+
+	switch(randomnumber)
+	{
+	case 1:
+	  $("#bigimage").css("backgroundImage", 'url(http://www2.binghamton.edu/photos/images/20110711_demo01_jwc.jpg)');
+	  break;
+	case 2:
+	  $("#bigimage").css("backgroundImage", 'url(http://www2.binghamton.edu/photos/images/20060315_sculpture08_jwc.jpg)');
+	  break;
+	case 3:
+	  $("#bigimage").css("backgroundImage", 'url(http://www2.binghamton.edu/photos/images/20110615_dc01_jwc.jpg)')
+	  break;
+	case 4:
+	  $("#bigimage").css("backgroundImage", 'url(http://www2.binghamton.edu/photos/images/20090528_anderson06_jwc.jpg)')
+	  break;
+	default:
+		
+	}
     // put all your jQuery goodness in here.
 	$('.hsubmit').hide();
+	
+	
 	
 	function popupCenter(url, width, height, name) {
 	  var left = (screen.width/2)-(width/2);
