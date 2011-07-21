52ff4333a10464422bbf17bd1f32820186aa871b
Kevin Urrutia <urrutia.kevin@gmail.com>
2011-07-21 18:53:15 -0400

    removed unused js

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,12 +1,6 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
-// jQuery.fn.center = function () {
-//     this.css("position","absolute");
-//     this.css("top", (($(window).height() - this.outerHeight()) / 2) + $(window).scrollTop() + "px");
-//     this.css("left", (($(window).width() - this.outerWidth()) / 2) + $(window).scrollLeft() + "px");
-//     return this;
-// }
 $(document).ready(function() {
 	var randomnumber=Math.floor(Math.random()*6)
 
@@ -30,53 +24,4 @@ $(document).ready(function() {
 	default:
 		
 	}
-	
-	// function loadMore(pageNo) {
-	//   var url = '/questions/page/';
-	//   $.get(url + pageNo, function(response) {
-	//     $("#attach").append(response);
-	//   });
-	// }
-	// 
-	// $(document).ready(function() {
-	//   var currPage = 1;
-	//   $("a.next").live("click",function()  {
-	//     loadMore(++currPage);
-	//   });
-	// });
-	// 
-	// $(window).scroll(function(){
-	//         if  ($(window).scrollTop() == $(document).height() - $(window).height()){
-	// 			var currPage = 1;
-	//             loadMore(++currPage);
-	//         }
-	// });
-//     // put all your jQuery goodness in here.
-// 	$('.hsubmit').hide();
-// 	
-// 	
-// 	
-// 	function popupCenter(url, width, height, name) {
-// 	  var left = (screen.width/2)-(width/2);
-// 	  var top = (screen.height/2)-(height/2);
-// 	  window.open(url, name, "menubar=no,toolbar=no,status=no,width="+width+",height="+height+",toolbar=no,left="+left+",top="+top);
-// 	  return window.close();
-// 	}
-// 
-// 	$("a.popup").click(function(e) {
-// 	  popupCenter($(this).attr("href"), $(this).attr("data-width"), $(this).attr("data-height"), "authPopup");
-// 	  return false;
-// 	});
-// 	
-// 	$(function(){
-// 	var pop = function(){
-// 		$('#screen').css({	"display": "block", opacity: 0.7, "width":$(document).width(),"height":$(document).height()});
-// 		$('#box').css({"display": "block"});
-// 	}
-// 	$('.pu').click(pop);
-// 	$(window).resize(function(){
-// 		$('#box').css("display") == 'block'?pop.call($('#button')):"";
-// 		});
-// 	});
-// 	$('.close').click(function(){$('#box').css("display", "none");$('#screen').css("display", "none")});
 });
