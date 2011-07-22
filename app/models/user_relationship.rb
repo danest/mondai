8dfe47ec293913580ca00877fa6231769849d780
Chelman <nor201@gmail.com>
2011-07-22 22:19:11 +0700

    Delete all unnessecary files

--- app/models/user_relationship.rb
+++
@@ -1,6 +0,0 @@
-class UserRelationship < ActiveRecord::Base
-  attr_accessible :user_followed_id
-
-  belongs_to :user_follower, :class_name => "User"
-  belongs_to :user_followed, :class_name => "User"
-end
