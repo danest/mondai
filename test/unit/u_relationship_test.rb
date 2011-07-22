1a35c5796c2a5d25b48bdff40cab86152c929565
Chelman <nor201@gmail.com>
2011-07-22 22:13:18 +0700

    Fixed name of files

--- test/unit/u_relationship_test.rb
+++ test/unit/u_relationship_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class UserRelationshipTest < ActiveSupport::TestCase
+class URelationshipTest < ActiveSupport::TestCase
   # Replace this with your real tests.
   test "the truth" do
     assert true
