9d7e05741a49972ea31e64e09c6cd82d980b777f
Kevin Urrutia <urrutia.kevin@gmail.com>
2011-07-24 09:50:08 -0400

    passing only the user id to mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,8 +2,8 @@ class UserMailer < ActionMailer::Base
   include Resque::Mailer
   default :from => "mondai@gmail.com"
   
-  def registration_confirmation(user)
-    @user = user
-    mail(:to => "#{user.name} <#{user.email}>", :subject => "Registered")
+  def registration_confirmation(user_id)
+    @user = User.find(user_id)
+    mail(:to => "#{@user.name} <#{@user.email}>", :subject => "Registered")
   end
 end
