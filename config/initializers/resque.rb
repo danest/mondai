5606da3d0d5af98c14fed3aceedef8c9ee3c89e1
Kevin Urrutia <urrutia.kevin@gmail.com>
2011-07-24 09:01:24 -0400

    added redistogo url

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,7 +1,7 @@
-ENV["REDISTOGO_URL"] ||= "redis://username:password@host:1234/"
-
+ENV["REDISTOGO_URL"] ||= "redis://redistogo:b99859dd8742c5cdcf23242a6f1bfd55@angler.redistogo.com:9000/"
 uri = URI.parse(ENV["REDISTOGO_URL"])
 Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+
 Dir["#{Rails.root}/app/workers/*.rb"].each { |file| require file }
 Resque::Server.use(Rack::Auth::Basic) do |user, password|
   password == "secret"
