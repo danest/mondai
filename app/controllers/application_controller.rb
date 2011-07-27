a125a91709dceccaba9a8f211c4907b76a789ba8
Kevin Urrutia <urrutia.kevin@gmail.com>
2011-07-27 19:41:06 -0400

    regex to match

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,7 @@ class ApplicationController < ActionController::Base
   def ensure_domain 
       url = request.url
       if url.include?('www.mondai.me')
-          url = url.gsub('www.mondai.com', 'mondai.me')
+          url = url.gsub(/^www.mondai.me/, 'mondai.me')
           puts url
       end
   end
