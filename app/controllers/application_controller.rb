5091f5a3f2124e5b9f445403ded025f49de279c8
Kevin Urrutia <urrutia.kevin@gmail.com>
2011-07-27 19:17:25 -0400

    rollback

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter :ensure_domain
   include SessionsHelper
 
   # Customize the Devise after_sign_in_path_for() for redirecct to previous page after login
@@ -15,13 +14,6 @@ class ApplicationController < ActionController::Base
     end
   end
   
-   def ensure_domain 
-      url = request.url
-      if url.include?('www.mondai.me')
-          url = url.gsub('www.mondai.me', 'http://mondai.me')
-      end
-      redirect_to url, :status => 301
-   end
   
   private
     def store_location
